3a546c600d1789904b1b677195a22d3f9586a897
Jordan <jordanleelong5@gmail.com>
2020-12-16 17:01:50 -0800

    finished solution for stack visualizer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+
+  const [stacker, setStacker] = useState([]); 
+  const [number, setNumber] = useState('');
+
+  const handleChange = (event) => {
+
+    setNumber(event.target.value); 
+
+  }
+
+  const handlePush = () => {
+
+    if (number === '') return; 
+
+    let newStack = [...stacker] // create a copy of our stacker array
+
+    newStack.unshift(number) // take the copy of our stacker array and add in number at the beginning.
+
+    setStacker(newStack) // set the state of stacker to our copied array
+
+    setNumber('') // clear the input field
+   
+
+  }
+
+  const handlePop = () => {
+
+    let popper = [...stacker] // create a copy of our stacker array again
+popper.shift()  // remove the element from the beginning of the array. 
+
+    setStacker(popper)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Imma Stack</h1>
+      <input type='number' onChange={handleChange} value={number} />
+      <button onClick={handlePush}>push</button>
+      <button onClick={handlePop}>pop</button>
+
+      <div>
+        {stacker.map(stack => <div className='stack-style' >{stack}</div>)}
+      </div>
+
+      
+    
     </div>
   );
 }
 
 export default App;
+
+
